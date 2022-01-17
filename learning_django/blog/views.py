fee11ef025657b5e670c64a4fa15d1111fd54f92
Federico Pizarro Bejarano <federico.pizarrobejarano@mail.utoronto.ca>
2022-01-16 21:44:48 -0500

    Removing unnecessary view

--- learning_django/blog/views.py
+++ learning_django/blog/views.py
@@ -6,14 +6,6 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from blog.models import Post
 
 
-def home(request):
-    context = {
-        'posts':Post.objects.all(),
-    }
-
-    return render(request, 'blog/home.html', context)
-
-
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
